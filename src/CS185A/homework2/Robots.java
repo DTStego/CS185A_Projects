fd14f5317a23b640d0efd2b631f51949605d4091
Jimmy Nguyen <dtstego@gmail.com>
2023-02-05 08:54:49 -0800

    Added a HashSet containing blocked index locations for grid that can't be visited. Useful for stopping unnecessary recursion calls although the efficacy of this method needs further research.

--- src/CS185A/homework2/Robots.java
+++ src/CS185A/homework2/Robots.java
@@ -4,9 +4,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Robots
 {
+    // Store blocked locations for a grid in a "row_column" format for lookup.
+    static HashSet<String> blockedLocations = new HashSet<>();
+
+    /**
+     *  Given a grid (char array) of size n x n with some obstacles marked on it where the robot cannot walk,
+     *  count the different ways the robot could go from the top left corner to the bottom right corner.
+     *  If none, test if it were possible if it could walk up and left as well. However, your program does
+     *  not handle very large numbers, so the answer should be given modulo 2^31 - 1.
+     *
+     * Example input:
+7
+......#
+####...
+.#.....
+.#...#.
+.#.....
+.######
+.#.....
+     * Output: THE GAME IS A LIE
+     */
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,7 +45,25 @@ public class Robots
 
         for (int i = 0; i < n; i++)
         {
+
             array[i] = br.readLine().toCharArray();
+
+            for (int j = 0; j < array[i].length; j++)
+            {
+                if (array[i][j] == '#')
+                {
+                    blockedLocations.add(i + "_" + j);
+                }
+            }
+        }
+
+        // Add in borders to blockedLocations.
+        for (int i = 0; i < n; i++)
+        {
+            blockedLocations.add("-1_" + i);
+            blockedLocations.add(i + "_-1");
+            blockedLocations.add(n + "_" + i);
+            blockedLocations.add(i + "_" + n);
         }
 
         int numOfRoutes;
@@ -47,38 +87,46 @@ public class Robots
 
     public static int recurseSearch(char[][] array, int rowIndex, int columnIndex)
     {
-        if (rowIndex < 0 || columnIndex < 0 || rowIndex >= array.length || columnIndex >= array[0].length)
-            return 0;
-
-        if (array[rowIndex][columnIndex] == '#')
-            return 0;
-
         if (rowIndex == array.length - 1 && columnIndex == array[0].length - 1)
             return 1;
 
+        int paths = 0;
 
-        return recurseSearch(array, rowIndex + 1, columnIndex)
-                + recurseSearch(array, rowIndex, columnIndex + 1);
+        if (!blockedLocations.contains(rowIndex + 1 + "_" + columnIndex))
+            paths += recurseSearch(array, rowIndex + 1, columnIndex);
+        if (!blockedLocations.contains(rowIndex + "_" + (columnIndex + 1)))
+            paths += recurseSearch(array, rowIndex, columnIndex + 1);
+
+        return paths;
     }
 
     public static boolean recurseSearchWithGodPowers(char[][] array, int rowIndex, int columnIndex)
     {
-        if (rowIndex < 0 || columnIndex < 0 || rowIndex >= array.length || columnIndex >= array[0].length)
-            return false;
-
-        if (array[rowIndex][columnIndex] == '#')
-            return false;
-
         if (array[rowIndex][columnIndex] == '.')
+        {
             array[rowIndex][columnIndex] = '#';
+            blockedLocations.add(rowIndex + "_" + columnIndex);
+        }
 
         if (rowIndex == array.length - 1 && columnIndex == array[0].length - 1)
             return true;
 
-        return recurseSearchWithGodPowers(array, rowIndex + 1, columnIndex)
-                || recurseSearchWithGodPowers(array, rowIndex, columnIndex + 1)
-                || recurseSearchWithGodPowers(array, rowIndex - 1, columnIndex)
-                || recurseSearchWithGodPowers(array, rowIndex, columnIndex - 1);
+        boolean accessible = false;
+
+        if (!blockedLocations.contains(rowIndex + 1 + "_" + columnIndex))
+            if (recurseSearchWithGodPowers(array, rowIndex + 1, columnIndex))
+                accessible = true;
+        if (!blockedLocations.contains(rowIndex + "_" + (columnIndex + 1)))
+            if (recurseSearchWithGodPowers(array, rowIndex, columnIndex + 1))
+                accessible = true;
+        if (!blockedLocations.contains(rowIndex - 1 + "_" + columnIndex))
+            if (recurseSearchWithGodPowers(array, rowIndex - 1, columnIndex))
+                accessible = true;
+        if (!blockedLocations.contains(rowIndex + "_" + (columnIndex - 1)))
+            if (recurseSearchWithGodPowers(array, rowIndex, columnIndex - 1))
+                accessible = true;
+
+        return accessible;
     }
 
     public static void printArray(char[][] array)
