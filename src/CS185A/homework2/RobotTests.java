b9761b03fb075fda6e0738a1b82f237ff5d4b810
Jimmy Nguyen <dtstego@gmail.com>
2023-02-07 11:53:10 -0800

    Created a basic test case for Robots on a grid.

--- src/CS185A/homework2/RobotTests.java
+++ src/CS185A/homework2/RobotTests.java
@@ -1,12 +1,48 @@
 package CS185A.homework2;
 
-import java.io.IOException;
-import java.util.Scanner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RobotTests
 {
-    public static void main(String[] args) throws IOException
-    {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
+    @BeforeEach
+    public void setUp()
+    {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown()
+    {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    void testCase1() throws IOException
+    {
+        String testCase = "7\n" +
+                ".....##\n" +
+                "####.##\n" +
+                ".......\n" +
+                "##.####\n" +
+                "...####\n" +
+                "#......\n" +
+                "###....\n";
+
+        InputStream is = new ByteArrayInputStream(testCase.getBytes(StandardCharsets.UTF_8));
+
+        Robots.setEnvironment(is);
+
+        assertEquals("THE GAME IS A LIE", outputStreamCaptor.toString().trim());
     }
 }
